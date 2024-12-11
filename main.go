467e435d52fb4511326c4efea683f91c57e365d7
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2024-12-11 02:28:09 -0600

    secrets

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ import (
 )
 
 const (
-	targetURL  = "https://www.falloutbuilds.com/fo76/nuke-codes/"
 	userAgent  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 	timeFormat = "01/02/2006, 03:04:05 PM"
 	maxRetries = 5                // Maximum number of retries
@@ -56,7 +55,7 @@ func NewClient() *HttpC {
 }
 
 // fetchDocument attempts to fetch the webpage and returns a parsed GoQuery document with retry logic
-func fetchDocument() (*goquery.Document, error) {
+func fetchDocument(targetURL string) (*goquery.Document, error) {
 	client := NewClient()
 
 	var doc *goquery.Document
@@ -158,8 +157,14 @@ func convertTimestampToTime(timestamp string) (string, error) {
 }
 
 func main() {
+	// Fetch the target URL from environment variables
+	targetURL := os.Getenv("TARGET_URL")
+	if targetURL == "" {
+		log.Fatalf("TARGET_URL is not set")
+	}
+
 	// Fetch the document with retry logic
-	doc, err := fetchDocument()
+	doc, err := fetchDocument(targetURL)
 	if err != nil {
 		log.Fatalf("Failed to fetch document: %v", err)
 	}
